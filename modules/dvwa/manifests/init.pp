013cf887ebe4cdc5bd8798b54276aa0722a6daa7
3rdDegree <3rddegree@server.fake>
2015-03-26 07:24:59 -0400

    Wget and unzip Damn Vulnerable Web App

--- modules/dvwa/manifests/init.pp
+++ modules/dvwa/manifests/init.pp
@@ -7,11 +7,28 @@ class dvwa {
         ensure => present,
     }
 
-    file { "/opt/downloads":
+    $download_dir = "/opt/downloads"
+    file { $download_dir:
         ensure => "directory",
     }
 
+    # File/directory names
+    $dvwa_ver = '1.0.8'
+    $dvwa_zip = 'v${dvwa_ver}.zip'
+    $dvwa_dir = "${download_dir}/DVWA-${dvwa_ver}"
+    $dvwa_url = "https://github.com/RandomStorm/DVWA/archive/${dvwa_zip}"
+
     # Download the Damn Vulnerable Web App
-    $dvwa_download = ''
+    exec {'wget-dvwa':
+        command => "wget ${dvwa_url} --directory-prefix=${download_dir}",
+        creates => "${download_dir}/${dvwa_zip}",
+        require => File[$download_dir],
+    }
+
+    exec {'unzip-dvwa':
+        command => "unzip -d ${download_dir} ${download_dir}/${dvwa_zip}",
+        creates => $dvwa_dir,
+        require => Exec['wget-dvwa'],
+    }
 
 }
