b7015734127dbbcf0785f13714fd462b8ceae358
3rdDegree <3rddegree@server.fake>
2015-03-25 21:54:53 -0400

    Add path for apt-get command

--- modules/apt/manifests/init.pp
+++ modules/apt/manifests/init.pp
@@ -1,6 +1,6 @@
 class apt {
     # Run the command
-    exec {'apt-get update': }
+    exec {'/usr/bin/apt-get update': }
 
     # Make Package resources dependant on apt-get update
     Exec['apt-get update'] -> Package <| provider == apt |>
