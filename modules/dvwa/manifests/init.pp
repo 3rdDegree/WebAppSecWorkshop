328f5603049d459bc618c3aa8d22945adbcd6371
3rdDegree <3rddegree@server.fake>
2015-03-25 22:27:10 -0400

    Add some packages for dvwa (wget, zip, unzip)

--- modules/dvwa/manifests/init.pp
+++ modules/dvwa/manifests/init.pp
@@ -1,7 +1,17 @@
 class dvwa {
+    package {[
+            'wget',
+            'zip',
+            'unzip',
+        ]:
+        ensure => present,
+    }
+
     file { "/opt/downloads":
         ensure => "directory",
     }
 
+    # Download the Damn Vulnerable Web App
+    $dvwa_download = ''
 
 }
