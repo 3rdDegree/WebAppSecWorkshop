95fb136538ce962404789af7999903aa5f6251df
3rdDegree <3rddegree@server.fake>
2015-03-27 07:25:25 -0400

    Copy DVWA files to the user's home directory

--- modules/dvwa/manifests/init.pp
+++ modules/dvwa/manifests/init.pp
@@ -15,7 +15,8 @@ class dvwa {
     # File/directory names
     $dvwa_ver = '1.0.8'
     $dvwa_zip = "v${dvwa_ver}.zip"
-    $dvwa_dir = "${download_dir}/DVWA-${dvwa_ver}"
+    $dvwa_dir = "DVWA-${dvwa_ver}"
+    $dvwa_path = "${download_dir}/${dvwa_dir}"
     $dvwa_url = "https://github.com/RandomStorm/DVWA/archive/${dvwa_zip}"
 
     # Download the Damn Vulnerable Web App
@@ -29,7 +30,7 @@ class dvwa {
     exec {'unzip-dvwa':
         command => "unzip -d ${download_dir} ${download_dir}/${dvwa_zip}",
         path    => '/usr/bin',
-        creates => $dvwa_dir,
+        creates => $dvwa_path,
         require => Exec['wget-dvwa'],
     }
 
--- modules/dvwa/manifests/user.pp
+++ modules/dvwa/manifests/user.pp
@@ -2,10 +2,21 @@
 # and a copy of the app
 
 define dvwa::user {
+    require dvwa
+
     user { $name:
         ensure     => present,
         managehome => true,
         groups     => "www-data",
         password   => "webappsec",
+    }
+
+    file {"/home/${name}/${dvwa::dvwa_dir}":
+        ensure  => directory,
+        source  => $dvwa::dvwa_path,
+        recurse => true,
+        owner   => $name,
+        group   => 'www-data',
+        require => User[$name],
     }
 }
