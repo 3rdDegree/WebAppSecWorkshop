c1dda97bcc3ac531c4c67a26330b15a886da3dfc
3rdDegree <3rddegree@server.fake>
2015-03-26 20:37:44 -0400

    Fix: double quotes, not singles!

--- modules/dvwa/manifests/init.pp
+++ modules/dvwa/manifests/init.pp
@@ -14,7 +14,7 @@ class dvwa {
 
     # File/directory names
     $dvwa_ver = '1.0.8'
-    $dvwa_zip = 'v${dvwa_ver}.zip'
+    $dvwa_zip = "v${dvwa_ver}.zip"
     $dvwa_dir = "${download_dir}/DVWA-${dvwa_ver}"
     $dvwa_url = "https://github.com/RandomStorm/DVWA/archive/${dvwa_zip}"
 
