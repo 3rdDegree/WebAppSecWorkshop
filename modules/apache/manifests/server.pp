f0aba73678148caecf1874244ea94f187e1cbf58
3rdDegree <3rddegree@server.fake>
2015-04-05 08:31:47 -0400

    Add additional required php packages

--- modules/apache/manifests/server.pp
+++ modules/apache/manifests/server.pp
@@ -1,12 +1,12 @@
 class apache::server {
 
-    $web_pkgs = ["apache2", "php5", "libapache2-mod-php5"]
+    $web_pkgs = ['apache2', 'php5', 'libapache2-mod-php5', 'php5-mysql', 'php-pear']
 
     package { $web_pkgs:
         ensure => installed,
     }
 
-    service { "apache2":
+    service { 'apache2':
         ensure     => running,
         enable     => true,
         hasstatus  => true,
